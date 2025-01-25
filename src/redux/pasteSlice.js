2e27cf3da7f0a04cee25791da6383bc2d1b1eb2d
abidsarkar <abidsarkar0@gmail.com>
2025-01-25 16:00:53 +0600

    all the handaling function in paste slice

--- src/redux/pasteSlice.js
+++ src/redux/pasteSlice.js
@@ -12,33 +12,42 @@ export const pasteSlice = createSlice({
   name: "paste",
   initialState,
   reducers: {
-    addToPastes: (state,action) => {
+    addToPastes: (state, action) => {
       const paste = action.payload;
       //add a check => paste already existed
-      state.pastes.push(paste)
-      localStorage.setItem("pastes",
-      JSON.stringify(state.pastes));
-      toast("paste is created successfully")
+      state.pastes.push(paste);
+      localStorage.setItem("pastes", JSON.stringify(state.pastes));
+      toast("paste is created successfully");
     },
-    updateToPastes: (state,action) => {
+    updateToPastes: (state, action) => {
       const paste = action.payload;
       const index = state.pastes.findIndex((item) => item._id === paste._id);
-      if(index >= 0){
+      if (index >= 0) {
         state.pastes[index] = paste;
         localStorage.setItem("pastes", JSON.stringify(state.pastes));
         toast.success("paste updated");
       }
     },
     resetAllPastes: (state, action) => {
-      
+      state.pastes = [];
+      localStorage.removeItem("pastes");
     },
-    removeFromPastes: (state,action) =>{
+    removeFromPastes: (state, action) => {
+      const pasteId = action.payload;
+      console.log(pasteId);
+      const index = state.pastes.findIndex((item) => item._id === pasteId);
 
+      if (index >= 0) {
+        state.pastes.splice(index, 1);
+        localStorage.setItem("pastes", JSON.stringify(state.pastes));
+        toast.success("paste deleted");
+      }
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { addToPastes, updateToPastes, resetAllPastes, removeFromPastes} = pasteSlice.actions;
+export const { addToPastes, updateToPastes, resetAllPastes, removeFromPastes } =
+  pasteSlice.actions;
 
 export default pasteSlice.reducer;
