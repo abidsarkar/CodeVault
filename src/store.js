b850e18a59662c2817e85df50457a92facd81913
abidsarkar <abidsarkar0@gmail.com>
2025-01-25 12:02:57 +0600

    store implement done

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { pasteReducer } from './redux/pasteSlice'
+import pasteReducer from './redux/pasteSlice'
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    paste: pasteReducer,
+  },
 })
