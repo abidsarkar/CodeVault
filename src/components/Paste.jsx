32315017472494a57955a374c0f3e5f7cb22005c
abidsarkar <abidsarkar0@gmail.com>
2025-01-25 16:17:43 +0600

    get all save data

--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Paste = () => {
+  const pastes = useSelector((state) => state.paste.pastes);
+  const [searchTerm, setSearchTerm] = useState("");
+  const dispatch = useDispatch();
+  const filteredData = pastes.filter((paste) =>
+    paste.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
-    <div>Paste</div>
-  )
-}
+    <div>
+      <input
+        className="p-2 rounded-2xl min-w-[300px] mt-5"
+        type="search"
+        placeholder="search here"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <div className="flex flex-col gap-5">
+        {filteredData.length > 0 &&
+          filteredData.map((paste) => {
+            return <div>{paste.title}</div>;
+          })}
+      </div>
+    </div>
+  );
+};
 
-export default Paste
+export default Paste;
